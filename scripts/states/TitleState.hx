6c74883199e37884ba45b33b06d7fb3c8e8cb6f7
AlejoGDOfficial <alejogdoff@gmail.com>
2026-05-12 20:08:43 -0500

    iba a hacer sopa de gato pero el gato cocino

--- scripts/states/TitleState.hx
+++ scripts/states/TitleState.hx
@@ -3,12 +3,34 @@ import funkin.visuals.objects.Bopper;
 
 using StringTools;
 
-core.debug.HotReloading.add('data/menus/title.json');
+HotReloading.add('data/menus/title.json');
+
+@:typedef JsonTitle = {
+    var directory:String;
+    
+    var logo:JsonSprite;
+    var gf:JsonSprite;
+    var enter:JsonSprite;
+
+    var enterPressAnimation:String;
+
+    var texts:Dynamic;
+    var textsOffset:Float;
+
+    var randomTexts:Array<String>;
+
+    var introDuration:Int;
+}
+
+@:typedef TextData = {
+    @:optional var text:Null<String>;
+    @:optional var offset:Float;
+};
 
 final config:JsonTitle = Paths.json('data/menus/title');
 
-var currentText(get, never):Null<String>;
-function get_currentText():Null<String>
+var currentTextData(get, never):Null<TextData>;
+function get_currentTextData():Null<TextData>
     return Reflect.field(config.texts, '' + Conductor.curBeat);
 
 function getRandomText():String
@@ -59,11 +81,11 @@ function onCreate()
         add(spr);
     }
 
-    text = new Alphabet(FlxG.width / 2, FlxG.height * 0.2);
+    text = new Alphabet(FlxG.width / 2, config.textsOffset);
     text.alignment = 'centered';
     add(text);
 
-    if (Conductor.music == null)
+    if (true || Conductor.music == null)
     {
         Conductor.play(Paths.music('freakyMenu'), CoolVars.meta.bpm);
 
@@ -76,8 +98,8 @@ function onCreate()
 var splitRandomText:Array<String> = [];
 var randomIndex:Int = 0;
 
-function addText(str:String)
-    text.text += (text.text == '' ? '' : '\n') + str;
+function setText(str:Null<String>)
+    text.text = str == '' ? '' : text.text + (text.text == '' ? '' : '\n') + str;
 
 function onSafeBeatHit(curBeat:Int)
 {
@@ -89,20 +111,19 @@ function onSafeBeatHit(curBeat:Int)
         
             camGame.flash(FlxColor.WHITE, Conductor.secSectionCrochet);
         } else {
-            if (config.randomTextsBeats.contains(curBeat))
+            if (currentTextData != null)
             {
-                if (randomIndex % 2 == 0)
+                if (currentTextData.text == null)
                 {
-                    splitRandomText = getRandomText();
+                    if (randomIndex % 2 == 0)
+                        splitRandomText = getRandomText();
 
-                    text.text = '';
+                    setText(splitRandomText[randomIndex++]);
+                } else {
+                    setText(currentTextData.text);
                 }
 
-                addText(splitRandomText[randomIndex++ % 2]);
-            } else if (currentText == '') {
-                text.text = '';
-            } else if (currentText != null) {
-                addText(currentText);
+                text.y = currentTextData.offset ?? config.textsOffset;
             }
         }
     }
